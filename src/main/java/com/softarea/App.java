d978ec58e92e043dfb71059b3fb0ccbc2cd72f80
lusarz <lukasz.usarz@gmail.com>
2015-08-26 00:03:51 +0200

    Initial config, empty window.

---
+++ src/main/java/com/softarea/App.java
@@ -0,0 +1,33 @@
+package com.softarea;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Hello world!
+ */
+public class App extends JFrame {
+
+    public App() {
+        initUI();
+    }
+
+    private void initUI() {
+
+        setTitle("Simple example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                App application = new App();
+                application.setVisible(true);
+            }
+        });
+    }
+}
