d17f26f2500831704c1f829201694b9caabd38da
lusarz <lukasz.usarz@gmail.com>
2015-09-01 09:55:48 +0200

    Init work on board.

--- src/main/java/com/softarea/tetris/engine/board/Board.java
+++ src/main/java/com/softarea/tetris/engine/board/Board.java
@@ -1,34 +1,42 @@
 package com.softarea.tetris.engine.board;
 
 import com.softarea.tetris.drawing.DrawingApi;
-import com.softarea.tetris.engine.blocks.Block;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Board {
+    static int DEFAULT_WIDTH = 10;
+    static int DEFAULT_HEIGHT = 20;
+
     private int width;
     private int height;
-    private DrawingApi drawingApi;
 
-    private Block actualBlock = null;
-    private List<BoardRow> rows = new ArrayList<BoardRow>();
+
+    DrawingApi drawingApi;
+
+    boolean[][] actualBlock;
+
+    int actualBlockPositionX;
+    int actualBlockPositionY;
+
+
+    boolean[][] gameCells;
+
+    //private Block actualBlock = null;
+    //private List<BoardRow> rows = new ArrayList<BoardRow>();
 
 
     public Board(int width, int height, DrawingApi drawingApi) {
         this.width = width;
         this.height = height;
         this.drawingApi = drawingApi;
-        initRows();
+        gameCells = new boolean[width][height];
     }
+
+    public Board(DrawingApi drawingApi) {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT, drawingApi);
+    }
+
 
     public void draw() {
         drawingApi.drawBoard(this);
-    }
-
-    private void initRows() {
-        for (int i = 0; i < height; i++) {
-            rows.add(new BoardRow(width));
-        }
     }
 }
---
+++ src/test/java/com/softarea/tetris/engine/board/BoardTest.java
@@ -0,0 +1,22 @@
+package com.softarea.tetris.engine.board;
+
+import junit.framework.TestCase;
+
+public class BoardTest extends TestCase {
+
+    public void testCellsEmptyAfterCreate() {
+        Board board = new Board(null);
+        boolean emptyCells = true;
+
+        for (int i = 0; i < board.gameCells.length; i++) {
+            boolean[] column = board.gameCells[i];
+            for (int j = 0; j < column.length; j++) {
+                if (board.gameCells[i][j]) {
+                    emptyCells = false;
+                    break;
+                }
+            }
+        }
+        assertTrue(emptyCells);
+    }
+}
